a35f73bc1b7109a900da95441ad60e00ec22bb58
Eduardo Augusto <eduz92337@gmail.com>
2026-06-12 17:27:55 -0300

    metodos ppm

--- ppm.hpp
+++ ppm.hpp
@@ -8,13 +8,31 @@ using namespace std;
 struct Ppm{
     uint8_t alfabeto[256];
     trie_contexto arvore;
+    vector<uint8_t> janela_atual;
     Codificador_aritmetico aritmetico;
     // Kmax e J vão ser passados como parâmetros da compilação
     int Kmax;
     int J;
-    Ppm(int k, int janela){
+    Ppm(int k, int tamanho){
         for(int i = 0;i<256;i++)alfabeto[i] = i;
         Kmax = k;
-        J = janela;
+        J = tamanho;
+    }
+    void le_nova_janela(){
+        // faz a leitura de novos J bytes
+
+    }
+    No* busca_maior_contexto(trie_contexto &arvore,vector<uint8_t>&janela){
+        // Busca o maior contexto possivel para aquela janela de bytes
+    }
+    void atualiza_frequencia_contexto(No* contexto,uint8_t byte){
+        // atualiza no contexto usado
+    }
+
+
+
+
+    pair<double,double> codifica_byte(){
+
     }
 };
