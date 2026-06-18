19da85f84d75057a85414fb6e1f887c4a218baf2
Eduardo Augusto <eduz92337@gmail.com>
2026-06-17 22:34:23 -0300

    percorre pastas dentro de pastas

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@
 #include"ppm.hpp"
 
 using namespace std;
+namespace fs = filesystem;
 
-void codifica_arquivo(ifstream &arquivo, Ppm &modelo, bool metricas){
+void codifica_arquivo(ifstream& arquivo, Ppm& modelo, bool metricas){
     char byte;
     while (arquivo.get(byte)){
         // processa o byte visto
@@ -20,11 +21,10 @@ void codifica_arquivo(ifstream &arquivo, Ppm &modelo, bool metricas){
     arquivo.close();
 }
 
-
-//  path, Kmax, J, opção de poda ou clear(0,1,2), opção de analise de metricas(1,0) 
+//  path, Kmax, J, opção de poda ou clear(0,1,2), opção de analise de metricas(1,0) comprime(1,0)
 int main(int argc , char* argv[]) {
-    if(argc < 6){
-        cout <<"Uso: ./a path_pasta k j adaptacao metricas\n";
+    if(argc < 7){
+        cout <<"Uso: ./a path_pasta k j adaptacao metricas comprime\n";
         return 1;
     }
     string path = argv[1];
@@ -32,29 +32,53 @@ int main(int argc , char* argv[]) {
     int j = atoi(argv[3]);
     int adaptacao  = atoi(argv[4]);
     bool metricas  = atoi(argv[5]) != 0;
+    bool comprime  = atoi(argv[6]) != 0;
 
-    Ppm modelo(k,j,adaptacao);
-    //para cada arquivo do diretório codifica o arquivo
-    for(auto arq : filesystem::directory_iterator(path)){
-        ifstream arquivo(arq.path(),ios::binary);
-        if(!arquivo.is_open()){
-            cout <<"Erro ao abrir o arquivo!"<<endl;
-            return 1;
+    if(comprime){
+        Ppm modelo(k,j,adaptacao);
+        uintmax_t tamanho_original = 0;
+        uintmax_t arquivos_count = 0;
+        error_code ec;
+
+        for(auto& entrada : fs::recursive_directory_iterator(path, fs::directory_options::skip_permission_denied, ec)){
+            if(ec){ 
+                cerr << "Erro: " << ec.message() << endl; return 1; 
+            }
+
+            if(fs::is_regular_file(entrada.status())){
+                tamanho_original += fs::file_size(entrada.path(), ec);
+                arquivos_count++;
+
+                ifstream arquivo(entrada.path(), ios::binary);
+                if(!arquivo.is_open()){
+                    cerr << "Erro ao abrir: " << entrada.path() << endl;
+                    return 1;
+                }
+                codifica_arquivo(arquivo, modelo, metricas);
+                //análise das métricas parciais(lf > lo*(1+p)? -> pode ou clear)
+            }
         }
-        codifica_arquivo(arquivo, modelo, metricas);
-        //análise das métricas parciais(lf > lo*(1+p)? -> pode ou clear)
+
+        cout << "[INFO] Arquivos encontrados: " << arquivos_count << endl;
+        cout << "[INFO] Tamanho original: " << tamanho_original << " bytes" << endl;
+
+        // flush final: emite os bits restantes para fechar o intervalo
+        modelo.aritmetico.finaliza_codificacao();
+
+        // Imprime estatísticas da trie
+        modelo.arvore.imprime_estatisticas_trie(k);
+
+        // salva o stream de bits resultante em arquivo
+        modelo.aritmetico.salva_arquivo("saida.bin");
+
+        cout << "[INFO] Tamanho comprimido: " << modelo.aritmetico.tamanho_comprimido() << " bytes" << endl;
+        cout << "Razão de Compressão: " 
+             << (100.0 *(1- (double)modelo.aritmetico.tamanho_comprimido() / tamanho_original))
+             << " %" << endl;
+
+    }else{
+        // Inicia o descompressor
     }
-
-    // flush final: emite os bits restantes para fechar o intervalo
-    modelo.aritmetico.finaliza_codificacao();
-
-    // Imprime estatísticas da trie
-    modelo.arvore.imprime_estatisticas_trie(k);
-
-    // salva o stream de bits resultante em arquivo
-    modelo.aritmetico.salva_arquivo("saida.bin");
-
-    cout << "Total de bits emitidos: " << modelo.aritmetico.tamanho_comprimido() * 8 << endl;
 
     return 0;
 }
